22f692615a27cb77e6a9af5febef160f1be9c83d
Alexander Spies <alexander.paul.spies@gmail.com>
2016-07-12 18:28:12 +0200

    added calories to build script

--- scripts/build_static_content.sh
+++ scripts/build_static_content.sh
@@ -2,5 +2,6 @@
 
 DIR=$(dirname $0)
 $DIR/../engine/reporting/build_spendings_output.py
+$DIR/../engine/reporting/build_calories_output.py
 #$DIR/../engine/reporting/build_hoursofwork_output.py
 #$DIR/../engine/reporting/build_workout_output.py
