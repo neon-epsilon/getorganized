df85edfe86be74e49c5258eab46bc9249256e352
Alexander Spies <alexander.paul.spies@gmail.com>
2017-04-02 00:24:56 +0200

    Changed order of shoppinglists.
    Shared shoppinglist first, then own.

--- page/shopping.php
+++ page/shopping.php
@@ -16,22 +16,22 @@ include $_SERVER["DOCUMENT_ROOT"] . '/design/menu.php';
 <div class="container">
 
     <div class="box">
-        <h1>Eigene Einkaufsliste</h1>
-        <?php
-        include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist_input_form.php';
-        ?>
-        <?php
-        include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist.php';
-        ?>
-    </div>
-
-    <div class="box">
         <h1>Gemeinsame Einkaufsliste</h1>
         <?php
         include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist_input_form_shared.php';
         ?>
         <?php
         include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist_shared.php';
+        ?>
+    </div>
+
+    <div class="box">
+        <h1>Eigene Einkaufsliste</h1>
+        <?php
+        include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist_input_form.php';
+        ?>
+        <?php
+        include $_SERVER["DOCUMENT_ROOT"] . '/engine/forms/shoppinglist.php';
         ?>
     </div>
 
