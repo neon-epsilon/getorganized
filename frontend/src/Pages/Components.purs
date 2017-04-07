371b16a302fdcf4347b2e38c82cb183d64e9b9dc
Alexander Spies <alexander.paul.spies@gmail.com>
2017-04-07 23:06:36 +0200

    Frontend: Updated Components.purs to Pux 8.6.

--- frontend/src/Pages/Components.purs
+++ frontend/src/Pages/Components.purs
@@ -1,92 +1,65 @@
 module Pages.Components where
 
-import Prelude (map)
+import Prelude (($))
+import Control.Bind (bind)
+import Data.Foldable (foldMap)
 
--- import Pux.Html as H
--- import Pux.Html.Attributes as A
+import Text.Smolder.HTML (div, span, form, ul, input, select, option)
+import Text.Smolder.HTML.Attributes (className, name, type', value, placeholder, required, step)
+import Text.Smolder.Markup (Attribute, Markup, attribute, text, (!))
 
-import Data.Array
+defaultValue :: String -> Attribute
+defaultValue = attribute "defaultValue"
 
 
--- type HtmlNode = forall action. Array (H.Html action) -> H.Html action 
--- type HtmlLeaf = forall action. H.Html action 
--- 
--- textLeaf :: 
---   (forall action. Array (H.Attribute action) -> Array (H.Html action) -> H.Html action)
---   -> String
---   -> HtmlLeaf
--- textLeaf element s = element [] [H.text s]
--- 
--- container :: HtmlNode
--- container = H.div [ A.className "container"]
--- 
--- box :: HtmlNode
--- box = H.div [ A.className "box"]
--- 
--- smallBox :: HtmlNode
--- smallBox = H.div [ A.className "small-box"]
--- 
--- 
--- h1 :: String -> HtmlLeaf
--- h1 = textLeaf H.h1
--- 
--- h2 :: String -> HtmlLeaf
--- h2 = textLeaf H.h2
--- 
--- img :: String -> HtmlLeaf
--- img s = H.img [ A.src s] []
--- 
--- 
--- form :: forall action. String -> Array (Array (H.Html action)) -> H.Html action
--- form submitName children =
---   H.form []
---     [ H.ul []
---       ( (map (H.li []) children) 
---       `snoc`
---         ( H.input
---           [ A.name submitName
---           , A.className "form_button"
---           , A.type_ "submit"
---           , A.value "Speichern"
---           ] []
---         )
---       )
---     ]
--- 
--- label :: String -> HtmlLeaf
--- label = textLeaf H.label
--- 
--- dateInput ::  HtmlLeaf
--- dateInput = 
---   H.input
---     [ A.type_ "date"
---     , A.name "date"
---     , A.defaultValue "2016-10-20"
---     , A.placeholder "2016-10-20"
---     , A.required true
---     ] []
--- 
--- numberInput ::  HtmlLeaf
--- numberInput = 
---   H.input
---     [ A.type_ "number"
---     , A.name "amount"
---     , A.step "0.01"
---     , A.required true
---     ] []
--- 
--- textInput ::  HtmlLeaf
--- textInput = 
---   H.input
---     [ A.type_ "text"
---     , A.name "comment"
---     ] []
--- 
--- formHint :: String -> HtmlLeaf
--- formHint s = H.span [ A.className "form_hint"] [ H.text s ]
--- 
--- select :: String -> Array {value :: String, text :: String} -> HtmlLeaf
--- select name options =
---   H.select [ A.name name ] (map makeOption options)
---   where
---     makeOption o = H.option [ A.value o.value ] [ H.text o.text ]
+container :: forall e. Markup e -> Markup e
+container = div ! className "container"
+
+box :: forall e. Markup e -> Markup e
+box = div ! className "box"
+
+smallBox :: forall e. Markup e -> Markup e
+smallBox = div ! className "small-box"
+
+
+customForm :: forall e. String -> Markup e -> Markup e
+customForm submitName children =
+  form $ ul $ do
+      children
+      input
+        ! name submitName
+        ! className "form_button"
+        ! type' "submit"
+        ! value "Speichern"
+
+dateInput :: forall e. Markup e
+dateInput = 
+  input
+    ! type' "date"
+    ! name "date"
+    ! defaultValue "2016-10-20"
+    ! placeholder "2016-10-20"
+    ! required "true"
+
+numberInput :: forall e. Markup e
+numberInput = 
+  input
+    ! type' "number"
+    ! name "amount"
+    ! step "0.01"
+    ! required "true"
+
+textInput :: forall e. Markup e
+textInput = 
+  input
+    ! type' "text"
+    ! name "comment"
+
+formHint :: forall e. String -> Markup e
+formHint s = span ! className "form_hint" $ text s
+
+customSelect :: forall e. String -> Array {value :: String, text :: String} -> Markup e
+customSelect selectName options =
+  select ! name selectName $ foldMap makeOption options
+  where
+    makeOption o = option ! value o.value $ text o.text
