f1bdfb7b993e4be7e25d710e0261b8e9804f37f6
Alexander Spies <alexander.paul.spies@gmail.com>
2017-04-19 00:36:22 +0200

    Frontend: POSTing form data now possible, typesafe date initialization.

--- frontend/src/Pages/HoursOfWork.purs
+++ frontend/src/Pages/HoursOfWork.purs
@@ -1,21 +1,23 @@
 module Pages.HoursOfWork where
 
-import Prelude (($), bind, map, show, pure, (>>=), (>=>), (<<<), const, (==), (<>), comparing)
+import Prelude (($), bind, map, show, pure, (>=>), (<<<), const, (==), (<>), comparing, otherwise)
+import Global (readFloat, isFinite)
+
 import Data.List (List(..), (:), sortBy)
-import Data.Either (Either(..), either, fromRight)
+import Data.Either (Either(..), either)
 import Data.Maybe (Maybe(..))
 
-import Data.Formatter.DateTime (formatDateTime)
+import Data.Formatter.DateTime (FormatterF (..), format)
+import Data.Functor.Mu (Mu (..))
 import Control.Comonad (extract)
-import Partial.Unsafe (unsafePartial)
 
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Now (NOW, nowDateTime)
 import Control.Monad.Aff (attempt, Aff)
 import Control.Monad.Aff.Console (log, CONSOLE)
 
-import Data.Argonaut (class DecodeJson, decodeJson, (.?), Json)
-import Network.HTTP.Affjax (AJAX, get)
+import Data.Argonaut (Json, class DecodeJson, decodeJson, (.?), (:=), (~>), jsonEmptyObject)
+import Network.HTTP.Affjax (AJAX, get, post)
 import Network.HTTP.StatusCode (StatusCode(..))
 
 import Pux (EffModel, noEffects)
@@ -54,14 +56,24 @@ type State =
   , categories :: List Category
   , formState :: FormState }
 
+
 data InitState = Initializing
   | Getting
   | Initialized
   | Error
 
+
 newtype Category = Category
   { category :: String
   , priority :: Int }
+
+instance decodeJsonCategory :: DecodeJson Category where
+  decodeJson json = do
+    obj <- decodeJson json
+    category <- obj .? "category"
+    priority <- obj .? "priority"
+    pure $ Category { category: category, priority: priority }
+
 
 type FormState =
   { date :: String
@@ -120,9 +132,17 @@ foldp Init state =
     [ do
       localeDateTime <- liftEff $ nowDateTime
       let dateTime = extract localeDateTime
-      -- formatDateTime returns a Left value if and only if the format string is invalid.
-      -- Therefore we may use fromRight here.
-      let dateString = unsafePartial $ fromRight $ formatDateTime "YYYY-MM-DD" dateTime
+      -- Formatter is a crazy functor fixpoint (Mu).
+      -- Therefore we construct the format using the Mu constructer "In".
+      -- Alternative:
+      -- let dateString = unsafePartial $ formRight $ formatDateTime "YYYY-MM-DD" dateTime
+      let isoFormat = In $ YearFull
+                    $ In $ Placeholder "-"
+                    $ In $ MonthTwoDigits
+                    $ In $ Placeholder "-"
+                    $ In $ DayOfMonthTwoDigits
+                    $ In End
+      let dateString = format isoFormat dateTime
       pure $ Just $ Form $ SetDate dateString
     , pure $ Just $ Ajax RequestCategories
     ]
@@ -142,12 +162,15 @@ foldp (Ajax (ReceiveCategories categories)) state@{ formState } =
       { initState = Error }
 foldp (Ajax RequestCategories) state =
   { state: state { initState = Getting }
-  , effects: [ fetchCategories ]
+  , effects: [ getCategories ]
   }
 
 foldp (Form (Submit ev)) state =
   { state: state
-  , effects: [ liftEff (preventDefault ev) >>= const (pure Nothing) ]
+  , effects: [ do
+    liftEff (preventDefault ev)
+    postEntry state.formState
+    ]
   }
 foldp (Form (DateChange ev)) state@{ formState } =
   noEffects $ state { formState = formState { date = targetValue ev } }
@@ -159,15 +182,15 @@ foldp (Form (SetDate d)) state@{ formState } =
   noEffects $ state { formState = formState { date = d } }
 
 
-fetchCategories :: forall eff. Aff (ajax :: AJAX, console :: CONSOLE | eff) (Maybe Event)
-fetchCategories = do
+getCategories :: forall eff. Aff (ajax :: AJAX, console :: CONSOLE | eff) (Maybe Event)
+getCategories = do
   r <- attempt $ get "/backend/api/hoursofwork/categories.php"
   case r of
     Left err -> do
       log $ show err
       pure $ Just $ Ajax AjaxError
     Right res | res.status == (StatusCode 200) -> do
-      let categories = decodeCategoriesResponse res.response
+      let categories = decodeCategories res.response
       either
         (log >=> const (pure $ Just $ Ajax AjaxError))
         (pure <<< Just <<< Ajax <<< ReceiveCategories)
@@ -179,23 +202,39 @@ fetchCategories = do
       either log log err
       pure $ Just (Ajax AjaxError)
 
-  where
-    decodeCategoriesResponse :: Json -> Either String (List Category)
-    decodeCategoriesResponse r = do
-      obj <- decodeJson r
-      categories <- obj .? "categories"
-      decodeJson categories
 
-    decodeErrorResponse :: Json -> Either String String
-    decodeErrorResponse r = do
-      obj <- decodeJson r
-      err <- obj .? "error"
-      decodeJson err
+postEntry :: forall eff. FormState -> Aff (ajax :: AJAX, console :: CONSOLE | eff) (Maybe Event)
+postEntry formState = do
+  r <- attempt $ post "/backend/api/hoursofwork/entries.php" $ encodeFormState formState
+  case r of
+    Left err -> do
+      log $ show err
+      pure $ Just $ Ajax AjaxError
+    Right res | res.status == (StatusCode 200) -> do
+      pure Nothing
+    -- | If status is not 200, we expect an object of the form {error: String}
+    Right res -> do
+      log $ "Error: Expected status 200, received " <> (\(StatusCode n) -> show n) res.status <> " while fetching categories."
+      let err = decodeErrorResponse res.response
+      either log log err
+      pure $ Just (Ajax AjaxError)
 
 
-instance decodeJsonCategory :: DecodeJson Category where
-  decodeJson json = do
-    obj <- decodeJson json
-    category <- obj .? "category"
-    priority <- obj .? "priority"
-    pure $ Category { category: category, priority: priority }
+decodeCategories :: Json -> Either String (List Category)
+decodeCategories r = do
+  obj <- decodeJson r
+  categories <- obj .? "categories"
+  decodeJson categories
+
+decodeErrorResponse :: Json -> Either String String
+decodeErrorResponse r = do
+  obj <- decodeJson r
+  err <- obj .? "error"
+  decodeJson err
+
+encodeFormState :: FormState -> Json
+encodeFormState formState =
+  "date" := formState.date
+  ~> "amount" := readFloat formState.amount
+  ~> "category" := formState.category
+  ~> jsonEmptyObject
