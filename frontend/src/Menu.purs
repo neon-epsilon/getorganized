9892473d07451b5992924bf1c195257f7eac5812
Alexander Spies <alexander.paul.spies@gmail.com>
2017-04-26 10:49:25 +0200

    Frontend/CSS: Reworked normal size menu, started reworking small menu.

--- frontend/src/Menu.purs
+++ frontend/src/Menu.purs
@@ -4,7 +4,7 @@ import Prelude (($), const)
 import Control.Bind (bind)
 
 import Text.Smolder.HTML (nav, span, input, label, ul, li, a)
-import Text.Smolder.HTML.Attributes (className, id, type', for, href)
+import Text.Smolder.HTML.Attributes (className, id, type', for)
 import Text.Smolder.Markup (text, (!), (#!))
 
 import Pux.DOM.HTML (HTML)
@@ -20,15 +20,26 @@ menu :: HTML Route
 menu = nav do
   input ! type' "checkbox" ! id "nav-checkbox-0"
   label ! for "nav-checkbox-0" $ do
-    span ! className "nav-name" $ text "GetOrganized"
-    span ! className "nav-icon" $ text "≡"
-  ul $ do
-    routeLink Home "Übersicht"
-    routeLink HoursOfWork "Arbeitszeit"
-    routeLink Spendings "Ausgaben"
-    routeLink Calories "Kalorien"
-    routeLink ShoppingList "Einkaufsliste"
+    span ! className "nav-header" $ do
+      span ! className "nav-name" $ text "GetOrganized"
+      span ! className "nav-icon" $ text "≡"
+    ul $ do
+      routeLink Home "Übersicht"
+      li $ do
+        span $ text "Test 1"
+        ul $ do
+          routeLink HoursOfWork "Arbeitszeit"
+          routeLink Spendings "Ausgaben"
+          li $ do
+            span $ text "Test 2"
+            ul $ do 
+              routeLink HoursOfWork "Arbeitszeit"
+              routeLink Spendings "Ausgaben"
+      routeLink HoursOfWork "Arbeitszeit"
+      routeLink Spendings "Ausgaben"
+      routeLink Calories "Kalorien"
+      routeLink ShoppingList "Einkaufsliste"
 
 routeLink :: Route -> String -> HTML Route
 routeLink r s =
-  li $ a ! className "route-link" #! onClick (const r) $ text s
+  li $ a #! onClick (const r) $ text s
