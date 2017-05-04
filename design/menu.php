5318236762ba1e81a7de64decf1dc173040fc2f8
Alexander Spies <alexander.paul.spies@gmail.com>
2017-05-04 11:49:35 +0200

    Flattened Menu and removed max-height transition for better usability.

--- design/menu.php
+++ design/menu.php
@@ -6,14 +6,9 @@
     <ul>
         <li><a href="/page/summary.php">Übersicht</a></li>
         <li>
-            <input type="checkbox" id="nav-checkbox-0-0">
-            <label for="nav-checkbox-0-0"><span class="nav-name">Details</span><span class="nav-icon">&#9660;</span></label>
-            <ul>
-                <li><a href="/page/hoursofwork.php">Arbeitszeit</a></li>
-                <li><a href="/page/spendings.php">Ausgaben</a></li>
-                <li><a href="/page/calories.php">Kalorien</a></li>
-                <!-- <li><a href="/page/workout.php">Training</a></li> -->
-            </ul>
+        <li><a href="/page/hoursofwork.php">Arbeitszeit</a></li>
+        <li><a href="/page/spendings.php">Ausgaben</a></li>
+        <li><a href="/page/calories.php">Kalorien</a></li>
         </li>
         <li><a href="/page/shopping.php">Einkaufsliste</a></li>
     </ul>
