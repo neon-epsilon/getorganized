e8e4d26b2fec161afc36805cca4753eea6f8e17b
Alexander Spies <alexander.paul.spies@gmail.com>
2019-02-17 13:54:42 +0100

    DB: Added sport as a calories category.

--- build/initial_data.sql
+++ build/initial_data.sql
@@ -1,6 +1,6 @@
 -- ---------- calories --------------
 LOCK TABLES `calories_categories` WRITE;
-INSERT INTO `calories_categories` VALUES ('Normales',1),('Getränke',2),('Snacks',3);
+INSERT INTO `calories_categories` VALUES ('Normales',1),('Getränke',2),('Snacks',3),('Sport',4);
 UNLOCK TABLES;
 
 LOCK TABLES `calories_goals` WRITE;
