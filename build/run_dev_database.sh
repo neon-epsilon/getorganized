e5e5c86876d018c374e1eb159bc51068d76c9e68
Alexander Spies <alexander.paul.spies@gmail.com>
2023-04-14 23:21:20 +0200

    Provide dev database docker capable of utf8

--- build/run_dev_database.sh
+++ build/run_dev_database.sh
@@ -3,4 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
-docker run --name getorganized_database --rm -e MYSQL_ROOT_PASSWORD=root -d -p 3306:3306 mysql:8.0
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+
+docker build -t getorganized_database $SCRIPT_DIR/dev_database
+# MYSQL_ROOT_PASSWORD needs to be set, otherwise the container errors on startup.
+docker run --name getorganized_database --rm -e MYSQL_ROOT_PASSWORD=root -d -p 3306:3306 getorganized_database
