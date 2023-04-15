8f4f405bd280830f3124de849922b9e265d8c17f
Alexander Spies <alexander.paul.spies@gmail.com>
2023-04-15 23:57:34 +0200

    Add new scripts for chart generation

---
+++ backend/generate_calories_output.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Use the charting service to generate charts.
+
+set -euo pipefail
+IFS=$'\n\t'
+set -x
+
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+
+docker run -it --rm -v $SCRIPT_DIR/../generated/:/usr/app/generated charting_service python ./generate_calories_output.py
