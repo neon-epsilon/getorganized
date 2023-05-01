2dc7f8cc16383f635df9e482db9a463d4bf6f065
Alexander Spies <alexander.paul.spies@gmail.com>
2023-05-01 15:17:30 +0200

    Implement endpoints to generate different charts

--- backend/charting_service/server.py
+++ backend/charting_service/server.py
@@ -11,7 +11,7 @@ async def run(cmd):
 
     await proc.wait()
 
-    if proc.returncode is not 0:
+    if proc.returncode != 0:
         raise ChildProcessError(f"Child process '{cmd}' terminated with status code {proc.returncode}.")
 
 class ChartType(str, Enum):
@@ -19,22 +19,30 @@ class ChartType(str, Enum):
     hoursofwork = "hoursofwork"
     spendings = "spendings"
 
-async def generate_chart(chart_type: ChartType,timestamp: Union[str, None] = None):
+async def generate_chart(chart_type: ChartType, timestamp: Union[str, None] = None):
     if timestamp is None:
         timestamp = ""
 
-    await run(f"python generate_{chart_type}_output.py {timestamp}")
+    await run(f"python generate_{chart_type.value}_output.py {timestamp}")
 
-async def generate_all(timestamp: Union[str, None] = None):
+async def generate_all_charts(timestamp: Union[str, None] = None):
     for chart_type in ChartType:
         await generate_chart(chart_type, timestamp)
 
 app = FastAPI()
 
+@app.post("/{chart_type}/")
+async def generate_chart_automatic_timestamp(chart_type: ChartType):
+    await generate_chart(chart_type, None)
+
+@app.post("/{chart_type}/{timestamp}")
+async def generate_chart_set_timestamp(chart_type: ChartType, timestamp: str):
+    await generate_chart(chart_type, timestamp)
+
 @app.post("/")
-async def generate_all_automatic_timestamp():
-    await generate_all(None)
+async def generate_all_charts_automatic_timestamp():
+    await generate_all_charts(None)
 
 @app.post("/{timestamp}")
-async def generate_all_set_timestamp(timestamp: str):
-    await generate_all(timestamp)
+async def generate_all_charts_set_timestamp(timestamp: str):
+    await generate_all_charts(timestamp)
