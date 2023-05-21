add87f388ac67e6910140fe2dc78f94572eea57b
Alexander Spies <alexander.paul.spies@gmail.com>
2023-05-21 11:21:35 +0200

    Add comment

--- backend/charting_service/server.py
+++ backend/charting_service/server.py
@@ -4,6 +4,12 @@ from enum import Enum
 import asyncio
 
 async def run(cmd):
+    # Running chart generation as subprocess is bad, also makes DoS attacks
+    # very easy without limiting the number of subprocesses that can be created
+    # this way.
+    # TODO Refactor such that chart generation scripts are directly called
+    # (potentially using a work queue so we do not have to wait so long for a
+    # response from the server).
     await asyncio.create_subprocess_shell(cmd)
 
 class ChartType(str, Enum):
