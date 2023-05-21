792840f8cb754726d28c4d1a74a1b9d96ac1feb2
Alexander Spies <alexander.paul.spies@gmail.com>
2023-05-21 12:04:37 +0200

    Remove obsolete script

--- backend/charting_service/generate_all_outputs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-IFS=$'\n\t'
-set -x
-
-python generate_calories_output.py >/dev/null 2>/dev/null
-python generate_spendings_output.py >/dev/null 2>/dev/null
-python generate_hoursofwork_output.py >/dev/null 2>/dev/null
