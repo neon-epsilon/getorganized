8111ef16af23fa0c6d494439699a1cae11160998
Alexander Spies <alexander.paul.spies@gmail.com>
2023-05-21 11:06:50 +0200

    Simplify server; timestamp via query parameters

--- backend/charting_service/server.py
+++ backend/charting_service/server.py
@@ -4,15 +4,7 @@ from enum import Enum
 import asyncio
 
 async def run(cmd):
-    proc = await asyncio.create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE)
-
-    await proc.wait()
-
-    if proc.returncode != 0:
-        raise ChildProcessError(f"Child process '{cmd}' terminated with status code {proc.returncode}.")
+    await asyncio.create_subprocess_shell(cmd)
 
 class ChartType(str, Enum):
     calories = "calories"
@@ -32,17 +24,9 @@ async def generate_all_charts(timestamp: Union[str, None] = None):
 app = FastAPI()
 
 @app.post("/{chart_type}/")
-async def generate_chart_automatic_timestamp(chart_type: ChartType):
-    await generate_chart(chart_type, None)
-
-@app.post("/{chart_type}/{timestamp}")
-async def generate_chart_set_timestamp(chart_type: ChartType, timestamp: str):
+async def generate_chart_endpoint(chart_type: ChartType, timestamp: Union[str, None] = None):
     await generate_chart(chart_type, timestamp)
 
 @app.post("/")
-async def generate_all_charts_automatic_timestamp():
-    await generate_all_charts(None)
-
-@app.post("/{timestamp}")
-async def generate_all_charts_set_timestamp(timestamp: str):
+async def generate_all_charts_endpoint(timestamp: Union[str, None] = None):
     await generate_all_charts(timestamp)
