2bb5f40c36715eb5f5acdb8f6e3faa0c72e624ec
Alexander Spies <alexander.paul.spies@gmail.com>
2023-05-21 15:26:44 +0200

    Add docker-compose.yml

--- build/build_containers.sh
+++ build/build_containers.sh
@@ -6,5 +6,5 @@ set -x
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$0")")
 
-docker build -t database_setup -f $SCRIPT_DIR/database_setup/Dockerfile .
-docker build -f $SCRIPT_DIR/../backend/charting_service/Dockerfile -t charting_service .
+docker build -t getorganized-database-setup -f $SCRIPT_DIR/database_setup/Dockerfile .
+docker build -t getorganized-charting-service -f $SCRIPT_DIR/../backend/charting_service/Dockerfile .
