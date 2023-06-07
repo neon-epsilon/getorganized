e2acff28899edba2b786e3880c8c7a6b6821123d
Alexander Spies <alexander.paul.spies@gmail.com>
2023-06-08 00:40:35 +0200

    Use generalized chart script for calories

--- charting_service/charts/__init__.py
+++ charting_service/charts/__init__.py
@@ -2,7 +2,6 @@ from typing import Union
 import pathlib
 import pymysql
 
-from charts import calories
 from charts import spendings
 from charts.generation import generate_charts
 from charts.amountsource import CaloriesAmountSource, HoursOfWorkAmountSource, SpendingsAmountSource
@@ -13,7 +12,7 @@ def generate_calories_charts(output_timestamp: Union[str, None]):
     calories_amount_source = CaloriesAmountSource(con)
 
     output_dir = pathlib.Path.cwd() / 'generated/calories'
-    calories.generate_charts(output_dir, calories_amount_source, output_timestamp)
+    generate_charts(output_dir, calories_amount_source, output_timestamp)
 
     con.close()
 
